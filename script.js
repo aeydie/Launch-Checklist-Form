4cb1e80b31a2a96c3d438161e5b6dffaebf13b7a
Eydie <you@example.com>
2021-06-18 20:41:25 -0500

    second draft

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 
    window.addEventListener("load", function() {
       fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-      response.json();.then(function(json) {
+      response.json().then(function(json) {
       const destination = document.getElementById("missionTarget");
-        <h2>Mission Destination</h2>`;
+      destination.innerHTML = `
+         <h2>Mission Destination</h2>
         <ol>
         <li>Name: ${json[3].name}</li>
         <li>Diameter: ${json[3].diameter}</li>
@@ -14,8 +15,9 @@
         <li>Number of Moons: ${json[3].moons}</li>
         </ol>
         <img src="${json[3].image}">
-        */
-      }
+        `;
+      });
+   });
  
       let form = document.querySelector("form");
       form.addEventListener("submit", function(event) {
@@ -23,48 +25,40 @@
 
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotNameInput = document.querySelector("input[name=copilotName]");
-      let fuelLevelInput = document.querySelector("input[name=fuelLevel");
-      let cargoMassInput = document.querySelector("input[name=cargoMass");
-      if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      let pilotStatusInput = document.getElementById("pilotStatus");
+      let copilotStatusInput = document.getElementById("copilotStatus");
+      let fuelStatusInput = document.getElementById("fuelStatus");
+      let cargoStatusInput = document.getElementById("cargoStatus");
+      let faultyItemsStatus = document.getElementById("faultyItems");
+      let newlaunchStatus = document.getElementById("launchStatus");
+
+      if (!isNaN(pilotNameInput) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
          alert("\nMake sure to enter valid information for each field!");
-         }
-      let pilotStatusInput = document.getElementById(pilotStatus);
-      let copilotStatusInput = document.getElementById(copilotStatus);
-      let fuelStatusInput = document.getElementById(fuelStatus);
-      let cargoStatusInput = document.getElementById(cargoStatus);
-      let fautlyItemsStatus = document.getElementById(faultyItems);
-      let newlaunchStatus = document.getElementById(launchStatus);
-      if (pilotNameInput ===  "" || copilotNameInput === "" || fuelLevelInput === "" || cargoMassInput === "") {
+         } else if (pilotNameInput.value ===  "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
          alert("\nAll fields are required!");
          }
-   
-     
-      
-      
-      function fuelStatusLevel() {
+
          if (fuelStatusInput.value < 10000) {
-            faultyItemsStatus.innerHTML = "Visible";
-            pilotStatusInput.innerHTML = `${pilotName.value} is ready for launch`;
-            copilotStatusInput.innerHTML =`${copilotName.value} is ready for launch`;
+            faultyItemsStatus.style.visibility = "Visible";
+            pilotStatusInput.innerHTML = `${pilotNameInput.value} is ready for launch`;
+            copilotStatusInput.innerHTML =`${copilotNameInput.value} is ready for launch`;
             newlaunchStatus.innerHTML = "Shuttle not ready for launch!";
             newlaunchStatus.style.color = "red";
          } else {
             newlaunchStatus.innerHTML = "Shuttle is ready to launch";
             newlaunchStatus.style.color = "green";
          }
-      }
-         
-
-         function cargoMassLevel() {
+      
             if (cargoMassInput.value > 10000) {
-               faultyItemsStatus.innerHTML = "Visible";
+               faultyItemsStatus.style.visibility = "Visible";
                cargoStatusInput.innerHTML = "There is too much cargo for the shuttle to take off";
                newlaunchStatus.innerHTML = "Shuttle not ready for launch";
                newlaunchStatus.style.color = "red";
             } else {
                newlaunchStatus.innerHTML = "Shuttle is ready to launch";
                newlaunchStatus.style.color = "green";
-            }
          }
       });  
    });
@@ -85,4 +79,3 @@
    <li>Number of Moons: ${}</li>
 </ol>
 <img src="${}"> */}
-*/
